c3037f8d43cd29cfc6cd685cbf74fe06af6f9ba1
MSWSal <sahansalgado10@gmail.com>
2023-07-28 09:54:36 +0530

    setup and rpad n lpad

---
+++ pong.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.setup(400,300)
+turtle.bgcolor("black")
+
+#Lpad
+Lpad = turtle.Turtle()
+Lpad.shape("square")
+Lpad.color("red")
+Lpad.shapesize(stretch_wid=5, stretch_len=1)
+Lpad.penup()
+Lpad.goto(-350, 0)
+Lpad.dy = 0
+
+#Rpad
+Rpad = turtle.Turtle()
+Rpad.shape("square")
+Rpad.color("yellow")
+Rpad.shapesize(stretch_wid=5, stretch_len=1)
+Rpad.penup()
+Rpad.goto(350, 0)
+Rpad.dy = 0
