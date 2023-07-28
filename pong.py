0fd01e0d3963b14ece0f28bb4ad9be5d3e4d48b6
MSWSal <sahansalgado10@gmail.com>
2023-07-28 10:13:41 +0530

    added game rules

--- pong.py
+++ pong.py
@@ -27,3 +27,15 @@ ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+
+#Rules
+gameover =Falsewinner =None
+points = {
+    "p1":0,
+    "p2":0
+}
+gamerules ={
+    "maxpoints": 3,
+    "ballspeed":3
+}
+
